cca9c104a41764be18e19b6c74bfc94371925ea5
henriquegranatto` <henrique.ramires.granatto@gmail.com`>
2020-11-04 17:32:58 -0300

    .

--- start.sh
+++ start.sh
@@ -9,28 +9,55 @@ set +a
 echo "----------------------------"
 echo "-- Atualidando o ambiente --"
 echo "----------------------------"
-sudo apt-get update -y
+apt-get update -y
+apt-get install -y nano git zip unzip
 
 echo "-------------------------"
 echo "-- Instalando o docker --"
 echo "-------------------------"
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get install -y apt-transport-https ca-certificates curl wget gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) \ stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo apt-get install docker-ce=$DOCKER_VERSION docker-ce-cli=$DOCKER_VERSION containerd.io
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update -y
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce=$DOCKER_VERSION docker-ce-cli=$DOCKER_VERSION containerd.io
 
 echo "--------------------------------"
 echo "-- Instalado o docker compose --"
 echo "--------------------------------"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# ALTERANDO AS PERMISSÕES DOS DIRETÓRIOS DOS VOLUMES
+echo "-----------------------------------------"
+echo "-- Alterando as permissões dos volumes --"
+echo "-----------------------------------------"
+cd search/volume/data && rm -rf * && cd /home/ubuntu/elasticsearch
+chmod -R 777 search/
+chmod -R 777 kibana/
+
+# REMOVE TODAS AS IMAGENS E CONTAINERS DA VM
+echo "------------------------------------"
+echo "-- Removendo resíduos de serviços --"
+echo "------------------------------------"
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
 
 # INICIANDO O ELASTICSEARCH, KIBANA E O ETL PELO DOCKER COMPOSE
-echo "------------------------------"
-echo "-- Incializando os serviços --"
-echo "------------------------------"
-sudo docker-compose up -d
+echo "-----------------------------------"
+echo "-- Incializando os serviços CORE --"
+echo "-----------------------------------"
+docker-compose up -d
